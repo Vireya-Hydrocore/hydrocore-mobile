21c2c0b052e96c8bfb4be5fa8e2114144deca679
SuricatoRDG123 <rodrigo.moreirasantosa@gmail.com>
2025-08-24 03:49:23 -0300

    Criação do Fragments Tarefa, sem conexão ao banco

---
+++ app/src/main/java/com/vireya/hydrocore/TarefasAdapter.java
@@ -0,0 +1,52 @@
+package com.vireya.hydrocore;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+import android.widget.TextView;
+
+
+public class TarefasAdapter extends RecyclerView.Adapter<TarefasAdapter.ViewHolder> {
+    private List<Tarefa> tarefas;
+
+    public TarefasAdapter(List<Tarefa> listaTarefas) {
+        this.tarefas = listaTarefas;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_tarefa, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Tarefa tarefa = tarefas.get(position);
+        holder.titulo.setText(tarefa.getTitulo());
+        holder.descricao.setText(tarefa.getDescricao());
+        holder.data.setText(tarefa.getPrazo().toString());
+    }
+
+    @Override
+    public int getItemCount() {
+        return tarefas.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView titulo, descricao, data;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            titulo = itemView.findViewById(R.id.txtTitulo);
+            descricao = itemView.findViewById(R.id.txtDescricao);
+            data = itemView.findViewById(R.id.txtPrazoData);
+        }
+    }
+}
+
