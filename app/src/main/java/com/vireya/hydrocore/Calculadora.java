3f294caa84c6e744578862232b216da874c9e934
SuricatoRDG123 <rodrigo.moreirasantosa@gmail.com>
2025-08-30 00:27:46 -0300

    Design Calculadora Realizado

--- app/src/main/java/com/vireya/hydrocore/Calculadora.java
+++ app/src/main/java/com/vireya/hydrocore/Calculadora.java
@@ -2,6 +2,7 @@ package com.vireya.hydrocore;
 
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,6 +10,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 
 public class Calculadora extends Fragment {
@@ -17,24 +23,45 @@ public class Calculadora extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        //Variáveis
         View view = inflater.inflate(R.layout.fragment_calculadora, container, false);
+        AutoCompleteTextView cbEtapa = view.findViewById(R.id.cbEtapa);
+        TextView txtProdutosQuimicos = view.findViewById(R.id.txtProdutosQuimicos);
+        TextInputLayout inputProdutosQuimicos = view.findViewById(R.id.inputProdutosQuimicos);
+        Button btnCalcular = view.findViewById(R.id.btnCalcular);
 
-        gerarComboBox(view);
+        //Cards
+        CardView cardCoagulacao = view.findViewById(R.id.cardCoagulacao);
+        CardView cardFloculacao = view.findViewById(R.id.cardFloculacao);
+
+
+
+        gerarComboEtapa(view, cbEtapa);
+
+        cbEtapa.setOnItemClickListener((parent, view1, position, id) -> {
+            String selecionado = (String) parent.getItemAtPosition(position);
+
+            if (selecionado.equals("Coagulação")) {
+                cardCoagulacao.setVisibility(View.VISIBLE);
+                cardFloculacao.setVisibility(View.GONE);
+            } else {
+                cardCoagulacao.setVisibility(View.GONE);
+                cardFloculacao.setVisibility(View.VISIBLE);
+            }
+
+            txtProdutosQuimicos.setVisibility(View.VISIBLE);
+            inputProdutosQuimicos.setVisibility(View.VISIBLE);
+            btnCalcular.setVisibility(View.VISIBLE);
+        });
+
+
 
         return view;
     }
 
     //Métodos GerarComboBox
-    private void gerarComboBox(View view) {
-        gerarComboCor(view);
-        gerarComboOdor(view);
-        gerarComboProdutosQuimicos(view);
-        gerarComboEtapa(view);
-    }
-
-    private void gerarComboEtapa(View view) {
-        AutoCompleteTextView cbEtapa = view.findViewById(R.id.cbEtapa);
-        String [] etapas = {"Coagulação", "Floculação", "Desinfecção", "Ajustes do pH"};
+    private void gerarComboEtapa(View view, AutoCompleteTextView cbEtapa) {
+        String [] etapas = {"Coagulação", "Floculação"};
 
         ArrayAdapter<String> adapterEtapas = new ArrayAdapter<>(
                 requireContext(),
@@ -49,54 +76,5 @@ public class Calculadora extends Fragment {
         });
     }
 
-    private void gerarComboProdutosQuimicos(View view) {
-        AutoCompleteTextView cbProdutosQuimicos = view.findViewById(R.id.cbProdutosQuimicos);
-        String [] produtosQuimicos = {"Polímero", "Sulfato de Alumínio", "Cal Hidratada"};
 
-        ArrayAdapter<String> adapterProdutosQuimicos = new ArrayAdapter<>(
-                requireContext(),
-                android.R.layout.simple_dropdown_item_1line,
-                produtosQuimicos
-        );
-
-        cbProdutosQuimicos.setAdapter(adapterProdutosQuimicos);
-
-        cbProdutosQuimicos.setOnClickListener(v -> {
-            cbProdutosQuimicos.showDropDown();
-        });
-    }
-
-    private void gerarComboOdor(View view) {
-        AutoCompleteTextView cbOdor = view.findViewById(R.id.cbOdor);
-        String [] odores = {"Sem odor", "Terroso", "Mofo", "Lodo", "Algás", "Metálico", "Sulfuroso"};
-
-        ArrayAdapter<String> adapterOdor = new ArrayAdapter<>(
-                requireContext(),
-                android.R.layout.simple_dropdown_item_1line,
-                odores
-        );
-
-        cbOdor.setAdapter(adapterOdor);
-
-        cbOdor.setOnClickListener(v -> {
-            cbOdor.showDropDown();
-        });
-    }
-
-    private void gerarComboCor(View view) {
-        AutoCompleteTextView cbCor = view.findViewById(R.id.cbCor);
-        String [] cores = {"Transparente", "Turva", "Amarela", "Esverdeada", "Marrom", "Acinzentada", "Preta"};
-
-        ArrayAdapter<String> adapterCor = new ArrayAdapter<>(
-                requireContext(),
-                android.R.layout.simple_dropdown_item_1line,
-                cores
-        );
-
-        cbCor.setAdapter(adapterCor);
-
-        cbCor.setOnClickListener(v -> {
-            cbCor.showDropDown();
-        });
-    }
 }
