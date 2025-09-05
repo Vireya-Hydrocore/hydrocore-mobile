3667c889b52c66e9af79f8c504525e599a00170e
clarabartolini <clarinhabartolini321@gmail.com>
2025-09-05 12:03:48 -0300

    Criação do design e logica do estoque

---
+++ app/src/main/java/com/vireya/hydrocore/estoque/api/ApiService.java
@@ -0,0 +1,13 @@
+package com.vireya.hydrocore.estoque.api;
+
+import com.vireya.hydrocore.estoque.model.Produto;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface ApiService {
+    @GET("produtos")
+    Call<List<Produto>> getProdutos();
+}
