cb501e0651c5b588826c7d3a6085a52ade2c2de1
SuricatoRDG123 <rodrigo.moreirasantosa@gmail.com>
2025-10-16 07:04:06 -0300

    Arrumando problema de teclado na calculadora, Criando ChatBot

---
+++ app/src/main/java/com/vireya/hydrocore/chatbot/ChatAdapter.java
@@ -0,0 +1,69 @@
+package com.vireya.hydrocore.chatbot;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.vireya.hydrocore.R;
+import java.util.List;
+
+public class ChatAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private static final int TYPE_USER = 1;
+    private static final int TYPE_BOT = 2;
+    private final List<Message> messages;
+
+    public ChatAdapter(List<Message> messages) {
+        this.messages = messages;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return messages.get(position).isUser() ? TYPE_USER : TYPE_BOT;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        if (viewType == TYPE_USER) {
+            View v = inflater.inflate(R.layout.item_message_user, parent, false);
+            return new UserViewHolder(v);
+        } else {
+            View v = inflater.inflate(R.layout.item_message_bot, parent, false);
+            return new BotViewHolder(v);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        Message message = messages.get(position);
+        if (holder instanceof UserViewHolder)
+            ((UserViewHolder) holder).tvMessage.setText(message.getText());
+        else
+            ((BotViewHolder) holder).tvMessage.setText(message.getText());
+    }
+
+    @Override
+    public int getItemCount() {
+        return messages.size();
+    }
+
+    static class UserViewHolder extends RecyclerView.ViewHolder {
+        TextView tvMessage;
+        UserViewHolder(View v) {
+            super(v);
+            tvMessage = v.findViewById(R.id.tvMessage);
+        }
+    }
+
+    static class BotViewHolder extends RecyclerView.ViewHolder {
+        TextView tvMessage;
+        BotViewHolder(View v) {
+            super(v);
+            tvMessage = v.findViewById(R.id.tvMessage);
+        }
+    }
+}
--- app/src/main/java/com/vireya/hydrocore/tarefas/api/TarefasApi.java
+++ app/src/main/java/com/vireya/hydrocore/tarefas/api/TarefasApi.java
@@ -8,14 +8,24 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.PATCH;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface TarefasApi {
+
+    @Headers({
+            "Authorization: Bearer teste233",
+            "X-User-Email: teste@email.com"
+    })
     @GET("v1/tarefas/listar-nome/{nome}")
     Call<List<Tarefa>> listarTarefasPorNome(@Path(value = "nome", encoded = true)String nome);
 
+    @Headers({
+            "Authorization: Bearer teste233",
+            "X-User-Email: teste@email.com"
+    })
     @PATCH("v1/tarefas/atualizar-status")
     Call<Void> atualizarStatus(@Body AtualizarStatusRequest request);
 
